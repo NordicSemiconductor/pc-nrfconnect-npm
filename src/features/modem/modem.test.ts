80d3d7f0b304abb40fe7d2bdc8cbcd784f9eec9e
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2022-11-07 15:19:42 +0100

    Clean up comments

--- src/features/modem/modem.test.ts
+++ src/features/modem/modem.test.ts
@@ -17,39 +17,6 @@ describe('modem', () => {
 
         expect(result).toBe(true);
     });
-
-    // it('should handle OK response', done => {
-    //     const [modem] = initialiseModem();
-
-    //     const okResponse = 'OK';
-    //     modem.onResponse((data, error) => {
-    //         try {
-    //             expect(data).toBe(okResponse);
-    //             expect(error).toBe(undefined);
-    //             modem.close(done);
-    //         } catch (e) {
-    //             done(e);
-    //         }
-    //     });
-    //     modem.write(okResponse);
-    // });
-
-    // it('should handle errors', done => {
-    //     const [modem] = initialiseModem();
-    //     const errorResponse = 'ERROR';
-
-    //     modem.onResponse((data, error) => {
-    //         try {
-    //             expect(data).toBe(errorResponse);
-    //             expect(error).toBe(errorResponse);
-    //             modem.close(done);
-    //         } catch (e) {
-    //             done(e);
-    //         }
-    //     });
-
-    //     modem.write(errorResponse);
-    // });
 });
 
 // SETUP
