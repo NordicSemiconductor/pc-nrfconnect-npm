525e9d753963930aed414068f1169f4537edd26a
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-01-24 12:17:54 +0100

    Fix Typos

--- src/components/SystemFeatures/SystemFeatures.tsx
+++ src/components/SystemFeatures/SystemFeatures.tsx
@@ -12,13 +12,13 @@ import './systemFeatures.scss';
 export default () => (
     <div className="systemFeatures-container">
         <div className="graph">
-            <Alert variant="info" label="nPM powerUP 0.1​ - Preview release! ">
+            <Alert variant="info" label="nPM PowerUP​ 0.1​ - Preview release! ">
                 This is an unsupported, experimental preview and it is subject
                 to major redesigns in the future.
             </Alert>
 
             <div className="systemFeatures-cards">
-                <p>TODO Placholder</p>
+                <p>TODO Placeholder</p>
             </div>
         </div>
     </div>
