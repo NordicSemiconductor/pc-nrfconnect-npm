365d7e2c4548d7c132d2adda3d02f5b7f6ccaca9
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-03-21 10:13:36 +0100

    Improvments

--- src/components/SidePanel/ConnectionStatus.tsx
+++ src/components/SidePanel/ConnectionStatus.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { CollapsibleGroup, Steppers } from 'pc-nrfconnect-shared';
 import { Step } from 'pc-nrfconnect-shared/src/Steppers/Steppers';
@@ -14,108 +14,68 @@ import {
     isSupportedVersion,
     isUsbPowered,
 } from '../../features/pmicControl/pmicControlSlice';
-import { isPaused } from '../../features/serial/serialSlice';
-
-const CONNECTION_SUCCESS_STATE: Step = {
-    title: 'CONNECTION',
-    state: 'success',
-    caption: 'Connected to EK',
-};
-const CONNECTION_OFFLINE_STATE: Step = {
-    title: 'CONNECTION',
-    caption: 'Offline Mode',
-};
-
-const SHELL_OFFLINE_STATE: Step = {
-    title: 'SHELL',
-};
-
-const SHELL_FREE_STATE: Step = {
-    title: 'SHELL',
-    state: 'success',
-    caption: 'Shell is free',
-};
-
-const SHELL_BUSY_STATE: Step = {
-    title: 'SHELL',
-    state: 'warning',
-    caption: 'Shell is busy',
-};
-
-const SHELL_WRONG_FW_STATE: Step = {
-    title: 'SHELL',
-    state: 'failure',
-    caption: 'Wrong firmware',
-};
-
-const PMIC_OFFLINE_STATE: Step = {
-    title: 'PMIC',
-};
-
-const PMIC_UNKNOWN_STATE: Step = {
-    title: 'PMIC',
-    state: 'active',
-    caption: 'PMIC connecting to PMIC',
-};
-
-const PMIC_NO_POWER_STATE: Step = {
-    title: 'PMIC',
-    state: 'failure',
-    caption: 'PMIC is not powered',
-};
-
-const PMIC_NO_USB_POWER_STATE: Step = {
-    title: 'PMIC',
-    state: 'warning',
-    caption: 'PMIC is not powered with USB',
-};
-
-const PMIC_SUCCESS_POWER_STATE: Step = {
-    title: 'PMIC',
-    state: 'success',
-    caption: 'Connected Successfully',
-};
+import { getShellParser, isPaused } from '../../features/serial/serialSlice';
 
 export default () => {
     // Handle trace state
+    const shellParser = useSelector(getShellParser);
     const pmicState = useSelector(getPmicState);
     const supportedVersion = useSelector(isSupportedVersion);
     const usbPowered = useSelector(isUsbPowered);
     const paused = useSelector(isPaused);
-    const [steps, setSteps] = useState<Step[]>([]);
 
-    useEffect(() => {
-        let connectionStep = CONNECTION_OFFLINE_STATE;
-        let shellStep = SHELL_OFFLINE_STATE;
-        let pmicStep = PMIC_OFFLINE_STATE;
+    const connectionStep: Step = {
+        title: 'CONNECTION',
+        caption: 'Offline Mode',
+    };
+    const shellStep: Step = {
+        title: 'SHELL',
+    };
+    const pmicStep: Step = {
+        title: 'PMIC',
+    };
 
-        if (pmicState !== 'ek-disconnected') {
-            connectionStep = CONNECTION_SUCCESS_STATE;
+    if (pmicState !== 'ek-disconnected') {
+        connectionStep.caption = 'Connected to EK';
+        connectionStep.state = 'success';
 
-            shellStep = SHELL_FREE_STATE;
-            if (paused) {
-                shellStep = SHELL_BUSY_STATE;
-            } else if (!supportedVersion) {
-                shellStep = SHELL_WRONG_FW_STATE;
-            }
+        shellStep.caption = 'Shell is free';
+        shellStep.state = 'success';
 
-            pmicStep = PMIC_SUCCESS_POWER_STATE;
-            if (pmicState === 'pmic-unknown') {
-                pmicStep = PMIC_UNKNOWN_STATE;
-            } else if (pmicState === 'pmic-disconnected') {
-                pmicStep = PMIC_NO_POWER_STATE;
-            } else if (!usbPowered) {
-                pmicStep = PMIC_NO_USB_POWER_STATE;
-            }
+        if (paused) {
+            shellStep.state = 'warning';
+            shellStep.caption = [
+                'Shell is busy',
+                {
+                    caption: 'unpause',
+                    action: () => shellParser?.unPause(),
+                },
+            ];
+
+            pmicStep.caption = 'Waiting on shell';
+            pmicStep.state = 'active';
+        } else if (!supportedVersion) {
+            shellStep.state = 'failure';
+            shellStep.caption = 'Wrong firmware';
+        } else if (pmicState === 'pmic-unknown') {
+            pmicStep.caption = 'Connecting...';
+            pmicStep.state = 'active';
+        } else if (pmicState === 'pmic-disconnected') {
+            pmicStep.caption = 'Not powered';
+            pmicStep.state = 'failure';
+        } else if (!usbPowered) {
+            pmicStep.caption = 'Not powered with USB';
+            pmicStep.state = 'warning';
+        } else {
+            pmicStep.state = 'success';
+            pmicStep.caption = 'In sync';
         }
-
-        setSteps([connectionStep, shellStep, pmicStep]);
-    }, [paused, pmicState, supportedVersion, usbPowered]);
+    }
 
     return (
         <CollapsibleGroup heading="Connection Status" defaultCollapsed={false}>
             <div className="connection-status-container">
-                <Steppers steps={steps} />
+                <Steppers steps={[connectionStep, shellStep, pmicStep]} />
             </div>
         </CollapsibleGroup>
     );
