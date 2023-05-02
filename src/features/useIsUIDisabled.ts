811ea79f7dc33b232e849189f5ba17a90da68cb3
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-05-02 14:24:24 +0200

    Refactor

---
+++ src/features/useIsUIDisabled.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+    getPmicState,
+    getProfilingState,
+    isSupportedVersion,
+} from './pmicControl/pmicControlSlice';
+import { isPaused } from './serial/serialSlice';
+
+export default () => {
+    const paused = useSelector(isPaused);
+    const supportedVersion = useSelector(isSupportedVersion);
+    const pmicState = useSelector(getPmicState);
+    const profilingState = useSelector(getProfilingState);
+
+    const [pauseFor100Ms, setPauseFor100Ms] = useState(paused);
+    const disabled =
+        (!supportedVersion && pmicState !== 'ek-disconnected') ||
+        pmicState === 'pmic-disconnected' ||
+        pmicState === 'pmic-unknown' ||
+        profilingState === 'Running' ||
+        pauseFor100Ms;
+
+    useEffect(() => {
+        const t = setTimeout(() => {
+            setPauseFor100Ms(paused);
+        }, 100);
+
+        return () => clearTimeout(t);
+    }, [paused]);
+
+    return disabled;
+};
