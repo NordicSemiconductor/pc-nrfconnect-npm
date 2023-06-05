d1d2d4ac2f7560b6c29834f5bcc11d8c4814df28
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-06-05 19:58:44 +0200

    Remove unused code

--- src/actions/deviceActions.ts
+++ src/actions/deviceActions.ts
@@ -8,19 +8,12 @@ import type { AutoDetectTypes } from '@serialport/bindings-cpp';
 import { createSerialPort, Device } from 'pc-nrfconnect-shared';
 import type { SerialPortOpenOptions } from 'serialport';
 
-import {
-    setAvailableSerialPorts,
-    setSelectedSerialport,
-    setSerialPort,
-    setShellParser,
-} from '../features/serial/serialSlice';
+import { setSerialPort, setShellParser } from '../features/serial/serialSlice';
 import { TAction } from '../thunk';
 
 export const closeDevice = (): TAction => dispatch => {
     dispatch(setShellParser(undefined));
     dispatch(setSerialPort(undefined));
-    dispatch(setAvailableSerialPorts([]));
-    dispatch(setSelectedSerialport(undefined));
 };
 
 export const openDevice =
@@ -29,16 +22,9 @@ export const openDevice =
         // Reset serial port settings
         const ports = device.serialPorts;
 
-        if (ports && ports?.length > 0) {
-            dispatch(
-                setAvailableSerialPorts(ports.map(port => port.comName ?? ''))
-            );
-        }
-
         if (ports) {
             const comPort = ports[0].comName; // We want to connect to vComIndex 0
             if (comPort) {
-                dispatch(setSelectedSerialport(comPort));
                 await dispatch(
                     setSerialPort(
                         await createSerialPort(
--- src/features/serial/serialSlice.ts
+++ src/features/serial/serialSlice.ts
@@ -29,15 +29,6 @@ const serialSlice = createSlice({
     name: 'serial',
     initialState,
     reducers: {
-        setAvailableSerialPorts: (state, action: PayloadAction<string[]>) => {
-            state.availableSerialPorts = action.payload;
-        },
-        setSelectedSerialport: (
-            state,
-            action: PayloadAction<string | undefined>
-        ) => {
-            state.selectedSerialport = action.payload;
-        },
         setSerialPort: (
             state,
             action: PayloadAction<SerialPort | undefined>
@@ -58,20 +49,10 @@ const serialSlice = createSlice({
 });
 
 export const getSerialPort = (state: RootState) => state.app.serial.serialPort;
-export const getSelectedSerialport = (state: RootState) =>
-    state.app.serial.selectedSerialport;
-export const getAvailableSerialPorts = (state: RootState) =>
-    state.app.serial.availableSerialPorts;
 export const getShellParser = (state: RootState) =>
     state.app.serial.shellParser;
-
 export const isPaused = (state: RootState) => state.app.serial.isPaused;
 
-export const {
-    setSerialPort,
-    setAvailableSerialPorts,
-    setSelectedSerialport,
-    setShellParser,
-    setIsPaused,
-} = serialSlice.actions;
+export const { setSerialPort, setShellParser, setIsPaused } =
+    serialSlice.actions;
 export default serialSlice.reducer;
