7202f3afbbf06762a4dfdf40cbc8196e3bcad255
kylebonnici <kylebonnici@hotmail.com>
2024-04-30 09:34:01 +0200

    Feedback

--- src/features/pmicControl/npm/npm1300/documentationPmic1300.tsx
+++ src/features/pmicControl/npm/npm1300/documentationPmic1300.tsx
@@ -820,19 +820,14 @@ export const documentation: Documentation = {
         ],
         IBATLIM: [
             {
-                title: (
-                    <>
-                        <span>IBAT</span>
-                        <span className="subscript">LIM</span>
-                    </>
-                ),
+                title: <span>Battery Discharge Current Limit</span>,
                 content: [
                     <p key="p1">
-                        For best fuel gauge accuracy it is advised to use
+                        For best fuel gauge accuracy, use
                         <span>IBAT</span>
                         <span className="subscript">LIM_LOW</span> for
                         applications with a lower maximum battery discharge
-                        current than 150mA.
+                        current than 150 mA.
                     </p>,
                 ],
             },
