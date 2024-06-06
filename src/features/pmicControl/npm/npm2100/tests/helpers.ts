cc3214300f2eb8cd93f24b33792230007c099901
Oystein Steimler <oystein@nyvri.net>
2024-06-06 14:42:30 +0200

    NCD-829: Move and fix LDO callback tests

--- src/features/pmicControl/npm/npm2100/tests/helpers.ts
+++ src/features/pmicControl/npm/npm2100/tests/helpers.ts
@@ -26,7 +26,7 @@ import {
 import { getNPM2100 } from '../pmic2100Device';
 
 export const PMIC_2100_BUCKS = [0, 1];
-export const PMIC_2100_LDOS = [0, 1];
+export const PMIC_2100_LDOS = [0];
 export const PMIC_2100_GPIOS = [0, 1, 2, 3, 4];
 export const PMIC_2100_LEDS = [0, 1, 2];
 
