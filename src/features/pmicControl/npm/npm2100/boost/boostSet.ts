8e328ab6089f23423af8516d84a520f6ccb46dbb
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2024-06-25 12:59:19 +0200

    refactor: split get and set into different files (boost)

--- src/features/pmicControl/npm/npm2100/boost/boostSet.ts
+++ src/features/pmicControl/npm/npm2100/boost/boostSet.ts
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-// eslint-disable-next-line max-classes-per-file
 import { NpmEventEmitter } from '../../pmicHelpers';
 import {
     Boost,
@@ -14,51 +13,7 @@ import {
     BoostPinSelection,
     PmicDialog,
 } from '../../types';
-
-export class BoostGet {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(
-        private sendCommand: (
-            command: string,
-            onSuccess?: (response: string, command: string) => void,
-            onError?: (response: string, command: string) => void
-        ) => void,
-        private index: number
-    ) {}
-
-    all() {
-        this.vOut();
-        this.mode();
-        this.modeControl();
-        this.pinSelection();
-        this.pinMode();
-        this.overCurrent();
-    }
-
-    vOut() {
-        this.sendCommand(`npm2100 boost vout get`);
-    }
-
-    mode() {
-        this.sendCommand(`npm2100 boost mode get`);
-    }
-
-    modeControl() {
-        this.sendCommand(`npm2100 boost voutsel get`);
-    }
-
-    pinSelection() {
-        this.sendCommand(`npm2100 boost pinsel get`);
-    }
-
-    pinMode() {
-        this.sendCommand(`npm2100 boost pinmode get`);
-    }
-
-    overCurrent() {
-        this.sendCommand(`npm2100 boost ocp get`);
-    }
-}
+import { BoostGet } from './boostGet';
 
 export class BoostSet {
     private get: BoostGet;
