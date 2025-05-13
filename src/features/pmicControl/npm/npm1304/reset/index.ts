dc2fedaa44939ef222aec24b8d0875a7d34707a8
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-05-13 12:53:34 +0200

    More code reuse from 1300

--- src/features/pmicControl/npm/npm1304/reset/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2025 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
- */
-
-import nPM1300ResetModule from '../../npm1300/reset';
-
-export default class Module extends nPM1300ResetModule {}
