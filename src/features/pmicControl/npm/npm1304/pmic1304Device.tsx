f35714708c63421f376ba96824afb8777501c7f0
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-05-26 17:08:19 +0200

    Hide Profile Button

--- src/features/pmicControl/npm/npm1304/pmic1304Device.tsx
+++ src/features/pmicControl/npm/npm1304/pmic1304Device.tsx
@@ -29,4 +29,9 @@ export default class Npm1304 extends nPM1300Device {
             npm1304FWVersion
         );
     }
+
+    // eslint-disable-next-line class-methods-use-this
+    get batteryProfiler() {
+        return undefined;
+    }
 }
