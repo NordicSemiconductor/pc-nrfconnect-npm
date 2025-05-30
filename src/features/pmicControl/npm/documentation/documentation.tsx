f1f402eadebaa332ef6b3f816ce4ef5f218f9cb5
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-05-30 14:36:38 +0200

    Fix tooltip ranges and nPM1300 referances to nPM1304

--- src/features/pmicControl/npm/documentation/documentation.tsx
+++ src/features/pmicControl/npm/documentation/documentation.tsx
@@ -9,10 +9,13 @@ import { useSelector } from 'react-redux';
 import { Overlay } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { getNpmDevice } from '../../pmicControlSlice';
+import BaseNpmDevice from '../basePmicDevice';
 import { documentation as pmic1300Documentation } from '../npm1300/documentationPmic1300';
+import Npm1300 from '../npm1300/pmic1300Device';
 import { documentation as pmic1304Documentation } from '../npm1304/documentationPmic1304';
+import Npm1304 from '../npm1304/pmic1304Device';
 import { documentation as pmic2100Documentation } from '../npm2100/documentationPmic2100';
-import { NpmModel } from '../types';
+import Npm2100 from '../npm2100/pmic2100Device';
 
 export const DocumentationTooltip = ({
     card,
@@ -29,7 +32,7 @@ export const DocumentationTooltip = ({
 }) => {
     const npmDevice = useSelector(getNpmDevice);
 
-    const fullDocumentation = getDocumentation(npmDevice?.deviceType);
+    const fullDocumentation = getDocumentation(npmDevice);
     const documentation =
         fullDocumentation && fullDocumentation[card]
             ? fullDocumentation[card]?.[item]
@@ -65,14 +68,16 @@ export const DocumentationTooltip = ({
     );
 };
 
-export const getDocumentation = (model: NpmModel | undefined) => {
-    switch (model) {
-        case 'npm1300':
-            return pmic1300Documentation;
-        case 'npm1304':
-            return pmic1304Documentation;
-        case 'npm2100':
-            return pmic2100Documentation;
+export const getDocumentation = (npmDevice: BaseNpmDevice | undefined) => {
+    if (npmDevice instanceof Npm2100) {
+        return pmic2100Documentation;
     }
+    if (npmDevice instanceof Npm1304) {
+        return pmic1304Documentation(npmDevice);
+    }
+    if (npmDevice instanceof Npm1300) {
+        return pmic1300Documentation(npmDevice);
+    }
+
     return null;
 };
