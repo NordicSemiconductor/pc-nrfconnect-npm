fd97c7ece00fb2bd9596f9099d0e1d181bb82441
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-07-03 10:15:38 +0200

    More Feedback

--- src/features/pmicControl/npm/npm1304/documentationPmic1304.tsx
+++ src/features/pmicControl/npm/npm1304/documentationPmic1304.tsx
@@ -190,16 +190,24 @@ export const documentation = (npmDevice: Npm1304): Documentation => {
                             menu below.
                         </p>,
                         <p key="p2">
-                            The nPM1304 EK is shipped with a 20 mAh LiPo
+                            The nPM1304 EK is shipped with a 20-mAh LiPo
                             battery. This is also the default battery model when
                             connecting to the kit. Battery electrical
                             specification:
                             <ul className="tw-ml-6 tw-list-disc">
                                 <li>Nominal voltage: 3.70 V</li>
-                                <li>Max charge voltage (Vterm): 4.20 V</li>
-                                <li>Max charge current (Ichg): 10 mA</li>
+                                <li>
+                                    Max charge voltage (V
+                                    <span className="subscript">term</span>):
+                                    4.20 V
+                                </li>
+                                <li>
+                                    Max charge current (I
+                                    <span className="subscript">chg</span>): 10
+                                    mA
+                                </li>
                                 <li>Max continuous discharge current: 60 mA</li>
-                                <li>Discharge Cut Off voltage: 2.75 V</li>
+                                <li>Discharge cut-off voltage: 2.75 V</li>
                             </ul>
                         </p>,
                     ],
