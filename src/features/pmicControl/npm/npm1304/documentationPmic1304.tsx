bc6f49cca3d99239ba869ae8937dfa890b737896
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-07-14 10:18:02 +0200

    Further impovments to tooltips

--- src/features/pmicControl/npm/npm1304/documentationPmic1304.tsx
+++ src/features/pmicControl/npm/npm1304/documentationPmic1304.tsx
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import { ExternalLink } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { documentation as nPM1300Documentation } from '../npm1300/documentationPmic1300';
 import { Documentation } from '../types';
@@ -209,6 +210,56 @@ export const documentation = (npmDevice: Npm1304): Documentation => {
                                 <li>Max continuous discharge current: 60 mA</li>
                                 <li>Discharge cut-off voltage: 2.75 V</li>
                             </ul>
+                        </p>,
+                    ],
+                },
+            ],
+            AddNewActiveBatteryModel: [
+                {
+                    title: 'Add New Active Battery Model',
+                    content: [
+                        <p key="p1">
+                            Nordic Semiconductor has profiled batteries from
+                            selected vendors. Choose a model from the drop-down
+                            menus below to add it to the{' '}
+                            <b>Active Battery Model</b> selection.
+                        </p>,
+                        <p key="p2">
+                            You can also add your own custom battery model. If
+                            your battery is not listed, click{' '}
+                            <b>Profile Battery</b> to create a new battery model
+                            and then add it using this menu.
+                        </p>,
+                        <p key="p3">
+                            For quick evaluation you can use one of the generic
+                            battery models, choose the one best matching the
+                            capacity and termination voltage of the battery
+                            used.
+                        </p>,
+                    ],
+                },
+            ],
+            ProfileBattery: [
+                {
+                    title: 'Profile Battery',
+                    content: [
+                        <p key="p1">
+                            {`Battery profiling provides accurate State of Charge
+                        estimation across voltage, current, and temperature
+                        range for the specific battery used. The result of the
+                        battery profiling is a battery model. To evaluate the
+                        custom battery model in nPM PowerUP, select the battery
+                        model in <p>Add New Active Battery Model</p> and write
+                        its JSON file to the selected battery model slot. To
+                        continue development and implementation on your own
+                        design, use the battery model INC file. See the
+                        following page for more details: `}
+                            <ExternalLink
+                                label={`${{
+                                    deviceType: npmDevice.deviceType,
+                                }}: Fuel gauge in the nRF Connect SDK documentation`}
+                                href={`https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/samples/pmic/native/${npmDevice.deviceType}_fuel_gauge/README.html`}
+                            />
                         </p>,
                     ],
                 },
