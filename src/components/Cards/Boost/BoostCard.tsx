feda91d4853af4c97ea09917e514e92faa89c185
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-08-12 22:17:28 +0200

    Remove logic from common UI

--- src/components/Cards/Boost/BoostCard.tsx
+++ src/components/Cards/Boost/BoostCard.tsx
@@ -159,20 +159,9 @@ export default ({
                         }
                         items={modeControlItems}
                         onSelect={item =>
-                            item.value === 'PASS'
-                                ? /* Decrease Vout below Vbat before forcing PT
-                                   * to avoid increased current consumption
-                                   */
-                                  boostModule.set
-                                      .vOut(range.min)
-                                      .then(() =>
-                                          boostModule.set.modeControl(
-                                              item.value as BoostModeControl
-                                          )
-                                      )
-                                : boostModule.set.modeControl(
-                                      item.value as BoostModeControl
-                                  )
+                            boostModule.set.modeControl(
+                                item.value as BoostModeControl
+                            )
                         }
                         selectedItem={
                             modeControlItems.find(
