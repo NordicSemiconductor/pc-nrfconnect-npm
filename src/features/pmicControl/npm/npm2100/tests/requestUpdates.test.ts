a16c29a3421bb2600480a4575c531381caef06be
Konsta Puutio <konsta.puutio@nordicsemi.no>
2025-10-10 14:59:38 +0300

    Move npm2100 ldo tests to module folder

--- src/features/pmicControl/npm/npm2100/tests/requestUpdates.test.ts
+++ src/features/pmicControl/npm/npm2100/tests/requestUpdates.test.ts
@@ -7,57 +7,13 @@
 import { ShellParserCallbacks as Callbacks } from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { npm2100FWVersion } from '../pmic2100Device';
-import { PMIC_2100_LDOS, setupMocksWithShellParser } from './helpers';
+import { setupMocksWithShellParser } from './helpers';
 
 describe('PMIC 2100 - Request update commands', () => {
     const { mockEnqueueRequest, pmic } = setupMocksWithShellParser();
 
     beforeEach(() => {
         jest.clearAllMocks();
-    });
-
-    test.skip('Request LDO Updates', () => {
-        test.each(PMIC_2100_LDOS)(
-            'Request update ldoVoltage index: %p',
-            index => {
-                pmic.ldoModule[index].get.voltage();
-
-                expect(mockEnqueueRequest).toBeCalledTimes(1);
-                expect(mockEnqueueRequest).toBeCalledWith(
-                    `npmx ldsw ldo_voltage get ${index}`,
-                    expect.anything(),
-                    undefined,
-                    true,
-                );
-            },
-        );
-
-        test.each(PMIC_2100_LDOS)(
-            'Request update ldoEnabled index: %p',
-            index => {
-                pmic.ldoModule[index].get.enabled();
-
-                expect(mockEnqueueRequest).toBeCalledTimes(1);
-                expect(mockEnqueueRequest).toBeCalledWith(
-                    `npmx ldsw status get ${index}`,
-                    expect.anything(),
-                    undefined,
-                    true,
-                );
-            },
-        );
-
-        test.each(PMIC_2100_LDOS)('Request update ldoMode index: %p', index => {
-            pmic.ldoModule[index].get.mode();
-
-            expect(mockEnqueueRequest).toBeCalledTimes(1);
-            expect(mockEnqueueRequest).toBeCalledWith(
-                `npmx ldsw mode get ${index}`,
-                expect.anything(),
-                undefined,
-                true,
-            );
-        });
     });
 
     test('Request startAdcSample', () => {
