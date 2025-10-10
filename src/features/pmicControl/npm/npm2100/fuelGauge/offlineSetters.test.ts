d51a5706a60ad9dd709083da9702af058db3bff2
Konsta Puutio <konsta.puutio@nordicsemi.no>
2025-10-10 08:51:46 +0300

    Edit setFuelGaugeEnable test
    - Remove unused dialog handler
    - Delete duplicate test in npm2100/tests/offlineSetters.test.ts

--- src/features/pmicControl/npm/npm2100/fuelGauge/offlineSetters.test.ts
+++ src/features/pmicControl/npm/npm2100/fuelGauge/offlineSetters.test.ts
@@ -4,23 +4,19 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { FuelGauge, PmicDialog } from '../../types';
+import { FuelGauge } from '../../types';
 import { setupMocksBase } from '../tests/helpers';
 
 describe('PMIC 2100 - Setters Offline tests', () => {
-    const { mockDialogHandler, mockOnFuelGaugeUpdate, pmic } = setupMocksBase();
+    const { mockOnFuelGaugeUpdate, pmic } = setupMocksBase();
 
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     test.each([true, false])(
-        'Set setFuelGuageEnable index: %p',
+        'Set setFuelGaugeEnable index: %p',
         async enabled => {
-            mockDialogHandler.mockImplementationOnce((dialog: PmicDialog) => {
-                dialog.onConfirm();
-            });
-
             await pmic.fuelGaugeModule?.set.enabled(enabled);
 
             expect(mockOnFuelGaugeUpdate).toBeCalledTimes(1);
