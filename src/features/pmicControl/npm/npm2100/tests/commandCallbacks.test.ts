b077e9b3de094b1f06443bd3234d4d1244d4fbf1
Konsta Puutio <konsta.puutio@nordicsemi.no>
2025-10-16 13:47:26 +0300

    Move npm2100 timer config tests to module folder

--- src/features/pmicControl/npm/npm2100/tests/commandCallbacks.test.ts
+++ src/features/pmicControl/npm/npm2100/tests/commandCallbacks.test.ts
@@ -4,12 +4,10 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { npm2100TimerMode } from '../types';
 import { setupMocksWithShellParser } from './helpers';
 
 describe('PMIC 2100 - Command callbacks', () => {
-    const { eventHandlers, mockOnTimerConfigUpdate, mockOnReboot } =
-        setupMocksWithShellParser();
+    const { eventHandlers, mockOnReboot } = setupMocksWithShellParser();
 
     beforeEach(() => {
         jest.clearAllMocks();
@@ -35,39 +33,6 @@ describe('PMIC 2100 - Command callbacks', () => {
 
         expect(mockOnReboot).toBeCalledTimes(1);
         expect(mockOnReboot).toBeCalledWith(false, 'Error: some message');
-    });
-
-    test.each(
-        Object.keys(npm2100TimerMode)
-            .map(mode => [
-                {
-                    append: `get`,
-                    mode,
-                },
-                {
-                    append: `set ${
-                        npm2100TimerMode[mode as keyof typeof npm2100TimerMode]
-                    }`,
-                    mode,
-                },
-            ])
-            .flat(),
-    )('npm2100 timer mode %p', ({ append, mode }) => {
-        const command = `npm2100 timer mode ${append}`;
-        const callback =
-            eventHandlers.mockRegisterCommandCallbackHandler(command);
-
-        callback?.onSuccess(
-            `${append === 'get' ? 'Value:' : 'Value:'} ${
-                npm2100TimerMode[mode as keyof typeof npm2100TimerMode]
-            }.`,
-            command,
-        );
-
-        expect(mockOnTimerConfigUpdate).toBeCalledTimes(1);
-        expect(mockOnTimerConfigUpdate).toBeCalledWith({
-            mode: npm2100TimerMode[mode as keyof typeof npm2100TimerMode],
-        });
     });
 });
 
