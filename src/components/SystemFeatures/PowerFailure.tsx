8a2b659eb24a6faef1de27ddece5b4ef9bf73f18
Konsta Puutio <konsta.puutio@nordicsemi.no>
2026-05-28 08:46:33 +0300

    SystemFeatures/PowerFailure.tsx: Sync with updated pof types

--- src/components/SystemFeatures/PowerFailure.tsx
+++ src/components/SystemFeatures/PowerFailure.tsx
@@ -7,6 +7,7 @@
 import React, { useEffect, useState } from 'react';
 import {
     Card,
+    classNames,
     Dropdown,
     NumberInput,
     Toggle,
@@ -17,28 +18,26 @@ import {
     type POF,
     type PofModule,
     type POFPolarity,
-    POFPolarityValues,
 } from '../../features/pmicControl/npm/types';
 
-interface GPIOProperties {
+interface PofProperties {
     pofModule: PofModule;
     pof: POF;
     disabled: boolean;
 }
 
-const pofPolarityValuesItems = POFPolarityValues.map(item => ({
-    label: `${item}`,
-    value: `${item}`,
-}));
-
-export default ({ pofModule, pof, disabled }: GPIOProperties) => {
-    const [internalPOFThreshold, setInternalPOFThreshold] = useState(
-        pof.threshold,
+export default ({ pofModule, pof, disabled }: PofProperties) => {
+    const [internalResetThreshold, setInternalResetThreshold] = useState(
+        pof.resetThreshold,
+    );
+    const [internalWarnThreshold, setInternalWarnThreshold] = useState(
+        pof.warnThreshold,
     );
 
     // NumberInputSliderWithUnit do not use pof.<prop> as value as we send only at on change complete
     useEffect(() => {
-        setInternalPOFThreshold(pof.threshold);
+        setInternalResetThreshold(pof.resetThreshold);
+        setInternalWarnThreshold(pof.warnThreshold);
     }, [pof]);
 
     const card = 'powerFailure';
@@ -51,14 +50,16 @@ export default ({ pofModule, pof, disabled }: GPIOProperties) => {
                         Power Failure
                     </DocumentationTooltip>
 
-                    <div className="d-flex">
-                        <Toggle
-                            label="POF Enabled"
-                            onToggle={v => pofModule.set.enabled(v)}
-                            disabled={disabled}
-                            isToggled={pof.enable}
-                        />
-                    </div>
+                    {pof.enabled !== undefined && (
+                        <div className="d-flex">
+                            <Toggle
+                                label="POF Enabled"
+                                onToggle={v => pofModule.set.enabled?.(v)}
+                                disabled={disabled}
+                                isToggled={pof.enabled}
+                            />
+                        </div>
+                    )}
                 </div>
             }
         >
@@ -74,33 +75,64 @@ export default ({ pofModule, pof, disabled }: GPIOProperties) => {
                 }
                 unit="V"
                 disabled={disabled}
-                range={pofModule.ranges.threshold}
-                value={internalPOFThreshold}
-                onChange={setInternalPOFThreshold}
-                onChangeComplete={v => pofModule.set.threshold(v)}
+                range={pofModule.ranges.resetThreshold}
+                value={internalResetThreshold}
+                onChange={setInternalResetThreshold}
+                onChangeComplete={v => pofModule.set.resetThreshold(v)}
             />
-            <Dropdown
-                label={
-                    <DocumentationTooltip card={card} item="POFPolarity">
-                        POF Polarity
+            {internalWarnThreshold !== undefined &&
+                pofModule.ranges.warnThreshold && (
+                    <NumberInput
+                        showSlider
+                        label={
+                            <DocumentationTooltip card={card} item="VSYSWARN">
+                                <>
+                                    VSYS
+                                    <span className="subscript">WARN</span>
+                                </>
+                            </DocumentationTooltip>
+                        }
+                        unit="V"
+                        disabled={disabled}
+                        range={pofModule.ranges.warnThreshold}
+                        value={internalWarnThreshold}
+                        onChange={setInternalWarnThreshold}
+                        onChangeComplete={v => pofModule.set.warnThreshold?.(v)}
+                    />
+                )}
+            {pofModule.values.polarity && pof.polarity !== undefined && (
+                <Dropdown
+                    label={
+                        <DocumentationTooltip card={card} item="POFPolarity">
+                            POF Polarity
+                        </DocumentationTooltip>
+                    }
+                    items={pofModule.values.polarity}
+                    onSelect={item =>
+                        pofModule.set.polarity?.(item.value as POFPolarity)
+                    }
+                    selectedItem={
+                        pofModule.values.polarity.find(
+                            item => item.value === pof.polarity,
+                        ) ?? { label: 'N/A', value: 'N/A' }
+                    }
+                    disabled={disabled}
+                />
+            )}
+            {pof.warnActive !== undefined && (
+                <div className="tw-flex tw-flex-row tw-gap-4">
+                    <DocumentationTooltip card={card} item="POFWarnActive">
+                        <div className="tw-text-xs">
+                            VSYS<span className="subscript">WARN</span>
+                        </div>
                     </DocumentationTooltip>
-                }
-                items={pofPolarityValuesItems}
-                onSelect={item =>
-                    pofModule.set.polarity(item.value as POFPolarity)
-                }
-                selectedItem={
-                    pofPolarityValuesItems[
-                        Math.max(
-                            0,
-                            pofPolarityValuesItems.findIndex(
-                                item => item.value === pof.polarity,
-                            ),
-                        ) ?? 0
-                    ]
-                }
-                disabled={disabled}
-            />
+                    <div
+                        className={`tw-h-4 tw-w-4 tw-rounded-full tw-border tw-border-solid tw-border-gray-200 ${classNames(
+                            pof.warnActive ? 'tw-bg-red' : 'tw-bg-green',
+                        )}`}
+                    />
+                </div>
+            )}
         </Card>
     );
 };
